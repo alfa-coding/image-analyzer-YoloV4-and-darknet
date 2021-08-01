90fce941c4c807a694e36447ba5a085d9d5d3d11
DimasVeliz <trevor90094@gmail.com>
2021-08-01 19:14:41 +0200

    repository dedicated only to darknet, tensorflow removed

--- test_darknet.py
+++ test_darknet.py
@@ -1,3 +1,4 @@
+import cv2
 from darknet_functionalities import loadClasses, loadNet,do_image_processing
 
 def test_CNNetworkload():
@@ -10,6 +11,7 @@ def test_loadClasses():
 def test_objectDetectionOverimage():
     newnet= loadNet("assets/yolov4-tiny.weights","assets/yolov4-tiny.cfg")
     classes= loadClasses("assets/coco.names")
-    dataCollected = do_image_processing(newnet,classes,"assets/catANDdog.jpg")
+    img= cv2.imread("assets/catANDdog.jpg")
+    dataCollected = do_image_processing(newnet,classes,img)
     expected=[{'class': 'cat', 'confidence': 0.8230796456336975, 'x': 67, 'y': 66, 'height': 912, 'width': 731}, {'class': 'dog', 'confidence': 0.7829499244689941, 'x': 555, 'y': 8, 'height': 860, 'width': 1407}]
     assert expected ==dataCollected
